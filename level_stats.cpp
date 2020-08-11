414e05f45310c13fad2f5fe2f096b89a671835ea
James <Icedshot@gmail.com>
2020-08-11 15:03:35 +0100

    untested level stats

--- level_stats.cpp
+++ level_stats.cpp
@@ -1,11 +1,48 @@
 #include "level_stats.hpp"
+#include <toml.hpp>
+#include <filesystem>
+#include <toolkit/fs_helpers.hpp>
 
 std::optional<level_stats::info> level_stats::load_best(const std::string& level_name)
 {
+    std::filesystem::create_directory("stats");
+    std::filesystem::create_directory("stats/" + level_name);
+
+    std::string data_file = "stats/" + level_name + "/stats.toml";
+
+    std::string data = file::read(data_file, file::mode::TEXT);;
+
+    std::istringstream is(data, std::ios_base::binary | std::ios_base::in);
+
+    try
+    {
+        toml::value val = toml::parse(is, data_file);
+
+        level_stats::info ret;
+        ret.assembly_length = toml::get<int>(val["assembly_length"]);
+        ret.cycles = toml::get<int>(val["cycles"]);
+
+        return ret;
+    }
+    catch(...)
+    {
+
+    }
+
     return std::nullopt;
 }
 
 void level_stats::save_best(const std::string& level_name, const level_stats::info& inf)
 {
+    std::filesystem::create_directory("stats");
+    std::filesystem::create_directory("stats/" + level_name);
+    std::string data_file = "stats/" + level_name + "/stats.toml";
 
+    toml::value val;
+    val["assembly_length"] = inf.assembly_length;
+    val["cycles"] = inf.cycles;
+
+    std::string as_str = toml::format(val);
+
+    file::write_atomic(data_file, as_str, file::mode::TEXT);
 }
